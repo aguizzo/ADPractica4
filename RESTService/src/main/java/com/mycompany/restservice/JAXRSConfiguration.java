ad24d78e910bf233feee1eccd896165b824db3d2
aguizzo <andres.leandro.guizzo@estudiantat.upc.edu>
2022-11-14 17:29:43 +0100

    configuracion de WS

--- RESTService/src/main/java/com/mycompany/restservice/JAXRSConfiguration.java
+++ RESTService/src/main/java/com/mycompany/restservice/JAXRSConfiguration.java
@@ -1,7 +1,10 @@
 package com.mycompany.restservice;
 
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
 
 /**
  * Configures JAX-RS for the application.
@@ -9,5 +12,17 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
-    
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        resources.add(MultiPartFeature.class);
+        return resources;
+    }      
+
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+    resources.add(com.mycompany.restservice.resources.JavaEE8Resource.class); 
+    //To change body of generated methods, choose Tools | Templates.     }
+    }   
 }
